ca67793b6f4ae4508de6828d94ec1873395ebc54
minami <minami.takanori.1113@gmail.com>
2024-11-21 07:53:31 +0900

    login後loginページの処理追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,45 @@
 import clsx from 'clsx'
 import type { Metadata } from 'next'
-
 import './login.scss'
 import { BackToTop } from '@/components/elements/btn/backToTop/BackToTop'
 import MotionWrap from '@/components/layouts/wrap/MotionWrap'
 import { LoginForm } from '@/features/login/components/loginForm/LoginForm'
+import { cookies } from 'next/headers'
+import axios from 'axios'
+import { authApiUrl, baseURL } from '@/utils/apiUrl'
+import { redirect } from 'next/navigation'
+import { routers } from '@/routers/routers'
 
 export const metadata: Metadata = {
   title: 'login',
 }
 
-const Profile = () => {
+const getAuth = async () => {
+  const cookie = cookies()
+    .getAll()
+    .map((cookie) => `${cookie.name}=${cookie.value}`)
+    .join('; ')
+  let user
+  try {
+    const res = await axios.get(`${baseURL}${authApiUrl.default}`, {
+      headers: { cookie },
+    })
+    if (res.data) {
+      user = res.data.userId
+    } else {
+      user = null
+    }
+  } catch (error) {
+    user = null
+  }
+  return user
+}
+
+const Profile = async () => {
+  const user = await getAuth();
+  if (user) {
+    redirect(routers.HOME);
+  }
   return (
     <MotionWrap>
       <div className={clsx('l-wrap', '-secondary')}>
